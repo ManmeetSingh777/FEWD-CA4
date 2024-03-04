65c3e3e4a033f1564b771f4666e5a2b74111dce2
ManmeetSingh777 <gmanmeetsingh888@gmail.com>
2024-03-04 12:50:05 +0530

    Project completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
       <div>
         <h1>Welcome to the Quiz!</h1>
-        <a href="/quiz">Play</a>
+        <Link to={"/quiz"}><button>Play</button></Link>
       </div>
     );
   }
